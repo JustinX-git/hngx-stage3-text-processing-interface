934bb1acaaf551297888167317d0b7242add2b24
Justin <justinjujec2@gmail.com>
2025-02-21 11:29:42 +0100

    summarization bug fix and refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,200 +1,128 @@
-// import { useState, useEffect, useRef} from "react";
-// import ChatSpace from "./components/ChatSpace/ChatSpace";
-// import "./App.css";
-
-// const App = ()  =>{
-//   const loadRef = useRef(0);
-//   const [errorState, setErrorState] = useState({isErr:false, msg:""});
-//   const [detectorState, setDetectorState] = useState({modelState:"unavailable",downloaded:0});
-//   const [summarizerState, setSummarizerState] = useState({modelState:"unavailable",downloaded:0});
-
-
-// //Checks and downloads the language detector model.
-// const downloadLangDetectModel = async() =>{
-//   const languageDetectorCapabilities = await self.ai.languageDetector.capabilities();
-//   const canDetect = languageDetectorCapabilities.available;
-
-//  if(canDetect === "no"){
-//   //The detector model is not available for use.
-//   setErrorState({isErr:true,msg:"This app uses features not supported by your device."})
-//   }else if(canDetect === "readily"){
-//   //The detector model is already available
-//      setDetectorState({modelState:"available",downloaded:100})
-//   }
-//   else {
-//     // Begin download of the detector model.
-//      const detector = await self.ai.languageDetector.create({
-//       monitor(m) {
-//         m.addEventListener("downloadprogress", (e) => {
-//           console.log(e.total,e.loaded);
-//           console.log(((e.total/e.loaded) * 100));
-//           setDetectorState({modelState:"downloading",downloaded:((e.total/e.loaded) * 100)})
-//         });
-//       },
-//     });
-//     await detector.ready;
-//   }
-// }
-
-// //Checks and downloads the summarizer model.
-// const downloadSummarizerModel =  async() =>{
-//   const languageSummarizerCapabilities = await self.ai.summarizer.capabilities();
-//   const canSummarize = languageSummarizerCapabilities.available;
-
-//   if (canSummarize === 'readily') {
-//     // The Summarizer API can be used immediately .
-//     setSummarizerState({modelState:"available",downloaded:100})
-
-//   } else if(canSummarize === 'after-download') {
-//   const summarizer = await self.ai.summarizer.create();
-
-//   summarizer.addEventListener('downloadprogress', (e) => {
-//     setSummarizerState({modelState:"downloading",downloaded:((e.loaded/e.total) * 100)})
-//   });
-//   await summarizer.ready;
-//   }
-// }
-
-
-//   // Both the translator and the summarizer API's are still very experimental with limited support however the language detector API requires much lesser resources and is more widely supported, hence the user's experience with the app is only interrupted if the language detector API is unavailable.
-// if(!errorState.isErr){
-//   if ('ai' in self && 'languageDetector' in self.ai){
-//     useEffect(()=>{
-//       try {
-//         downloadLangDetectModel();
-//       } catch (error) {
-//         setErrorState({isErr:true,msg:"Something went wrong while loading models."})
-//       }
-//     },[])    
-//   }else{
-//     setErrorState({isErr:true,msg:"Your browser does not support this application"})
-//   }
-// }
-
-// //The summarize API does not halt interaction with the app if not available or upon failure to download.
-// useEffect(()=>{
-//   downloadSummarizerModel();
-//  },[])
- 
-
-// //On instance of language detector not being usable
-//  if(errorState.isErr){
-//   return(
-//     <>
-//        <div className="compatibility-error-container">
-//         <div className="background"><i className="fa fa-frown-o" aria-hidden="true"></i></div>
-//        <h2 aria-label="error message" aria-live="assertive">{errorState.msg}</h2>
-//        </div>
-//     </>
-//   )
-// } 
-
-
-// //On instance of model downloads.
-//  if(detectorState.modelState === "downloading" || summarizerState.modelState === "downloading"){
-//   return(
-//     <>
-//     <div className="download-content-container">
-//     <h4>{`${detectorState.downloaded === 100 ? "Loaded detector model!" : `Downloading detector model:${detectorState.downloaded}%`}`}</h4>
-//     <h4>{`${summarizerState.downloaded === 100 ? "Loaded summarizer model!" : `Downloading summarizer model:${summarizerState.downloaded}`}%`}</h4>
-//     </div>
-//     </>
-//   )
-// }else if(detectorState.modelState === "available"){
-//   return (
-//     <>
-//       <ChatSpace/>
-//     </>
-//   );
-
-// }
-
-   
-
-
-// }
-
-// export default App;
-
 import { useState, useEffect } from "react";
 import ChatSpace from "./components/ChatSpace/ChatSpace";
 import "./App.css";
 
-const App = ()  =>{
-  const [errorState, setErrorState] = useState({isErr:false, msg:""});
-  const [detectorState, setDetectorState] = useState({modelState:"unavailable",downloaded:0});
-  const [summarizerState, setSummarizerState] = useState({modelState:"unavailable",downloaded:0});
+const App = () => {
+  const [errorState, setErrorState] = useState({ isErr: false, msg: "" });
+  const [detectorState, setDetectorState] = useState({
+    modelState: "unavailable",
+    downloaded: 0,
+  });
+  const [summarizerState, setSummarizerState] = useState({
+    modelState: "unavailable",
+    downloaded: 0,
+  });
 
   // Check and download the language detector model.
-  const downloadLangDetectModel = async() => {
-    const languageDetectorCapabilities = await self.ai.languageDetector.capabilities();
+  const downloadLangDetectModel = async () => {
+    const languageDetectorCapabilities =
+      await self.ai.languageDetector.capabilities();
     const canDetect = languageDetectorCapabilities.available;
 
     if (canDetect === "no") {
-      setErrorState({isErr:true, msg:"This app uses features not supported by your device."});
+      setErrorState({
+        isErr: true,
+        msg: "This app uses features not supported by your device.",
+      });
     } else if (canDetect === "readily") {
-      setDetectorState({modelState:"available", downloaded:100});
+      setDetectorState({ modelState: "available", downloaded: 100 });
     } else {
       const detector = await self.ai.languageDetector.create({
         monitor(m) {
           m.addEventListener("downloadprogress", (e) => {
-            setDetectorState({modelState:"downloading", downloaded: ((e.loaded / e.total) * 100)});
+            setDetectorState({
+              modelState: "downloading",
+              downloaded: (e.loaded / e.total) * 100,
+            });
           });
         },
       });
       await detector.ready;
-      setDetectorState({modelState:"available", downloaded:100});
+      setDetectorState({ modelState: "available", downloaded: 100 });
     }
   };
 
   // Check and download the summarizer model.
-  const downloadSummarizerModel = async() => {
-    const languageSummarizerCapabilities = await self.ai.summarizer.capabilities();
+  const downloadSummarizerModel = async () => {
+    const languageSummarizerCapabilities =
+      await self.ai.summarizer.capabilities();
     const canSummarize = languageSummarizerCapabilities.available;
 
-    if (canSummarize === 'readily') {
-      setSummarizerState({modelState:"available", downloaded:100});
-    } else if (canSummarize === 'after-download') {
+    if (canSummarize === "readily") {
+      setSummarizerState({ modelState: "available", downloaded: 100 });
+    } else if (canSummarize === "after-download") {
       const summarizer = await self.ai.summarizer.create();
-      summarizer.addEventListener('downloadprogress', (e) => {
-        setSummarizerState({modelState:"downloading", downloaded: ((e.loaded / e.total) * 100)});
+      summarizer.addEventListener("downloadprogress", (e) => {
+        setSummarizerState({
+          modelState: "downloading",
+          downloaded: (e.loaded / e.total) * 100,
+        });
       });
       await summarizer.ready;
-      setSummarizerState({modelState:"available", downloaded:100});
+      setSummarizerState({ modelState: "available", downloaded: 100 });
     }
   };
 
-  // UseEffect to check and load models
+  // The app would halt on failing to download the language detector model as this requires the least resources of the three, so a failure on this end almost certainly implies a failure on the other models. The reliance of the translator model on this API is another reason for this halting.
   useEffect(() => {
-    if ('ai' in self && 'languageDetector' in self.ai) {
+    if ("ai" in self && "languageDetector" in self.ai) {
       downloadLangDetectModel().catch(() =>
-        setErrorState({ isErr: true, msg: "Something went wrong while loading models." })
+        setErrorState({
+          isErr: true,
+          msg: "Something went wrong while loading models.",
+        })
       );
     } else {
-      setErrorState({ isErr: true, msg: "Your browser does not support this application" });
+      setErrorState({
+        isErr: true,
+        msg: "Your browser does not support this application",
+      });
     }
   }, []);
 
+  //Unlike the language detector model, the app would not halt on failing to download the summarize model due to how resource intensive it is.
   useEffect(() => {
-    downloadSummarizerModel();
+    downloadSummarizerModel().catch(() =>
+      setSummarizerState((prevState) => {
+        return {
+          ...prevState,
+          modelState: "unavailable",
+        };
+      })
+    );
   }, []);
 
   // Handle errors
   if (errorState.isErr) {
     return (
       <div className="compatibility-error-container">
-        <div className="background"><i className="fa fa-frown-o" aria-hidden="true"></i></div>
-        <h2 aria-label="error message" aria-live="assertive">{errorState.msg}</h2>
+        <div className="background">
+          <i className="fa fa-frown-o" aria-hidden="true"></i>
+        </div>
+        <h2 aria-label="error message" aria-live="assertive">
+          {errorState.msg}
+        </h2>
       </div>
     );
   }
 
   // Handle model downloads
-  if (detectorState.modelState === "downloading" || summarizerState.modelState === "downloading") {
+  if (
+    detectorState.modelState === "downloading" ||
+    summarizerState.modelState === "downloading"
+  ) {
     return (
       <div className="download-content-container">
-        <h4>{detectorState.downloaded === 100 ? "Loaded detector model!" : `Downloading detector model: ${detectorState.downloaded}%`}</h4>
-        <h4>{summarizerState.downloaded === 100 ? "Loaded summarizer model!" : `Downloading summarizer model: ${summarizerState.downloaded}%`}</h4>
+        <h4>
+          {detectorState.downloaded === 100
+            ? "Loaded detector model!"
+            : `Downloading detector model: ${detectorState.downloaded}%`}
+        </h4>
+        <h4>
+          {summarizerState.downloaded === 100
+            ? "Loaded summarizer model!"
+            : `Downloading summarizer model: ${summarizerState.downloaded}%`}
+        </h4>
       </div>
     );
   }
@@ -204,4 +132,3 @@ const App = ()  =>{
 };
 
 export default App;
-
